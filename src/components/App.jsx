eb6ef9535bf819f4d32638ac0c4a2e41d244fb95
stasikrus <stas-boss@mail.ru>
2023-10-20 18:55:17 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import Header from './Header'
+import TableComponent from './Table'
+import Container from '@mui/material/Container';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Container maxWidth="xl">
+        <TableComponent  />    
+      </Container>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+
+function Header() {
+  return (
+    <Box mb={3}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Разработка
+          </Typography>
+          <Button color="inherit">Войти</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
